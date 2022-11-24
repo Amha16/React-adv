1a2a2069d8958ab232ab3b63780ba09a92a5d9d5
Amha16 <capamha@gmail.com>
2022-11-24 16:47:27 +0300

    context Api added on navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/Context/auth_context';
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  useEffect(()=>{
+    
+    const logg =  localStorage.getItem('loginstatus');
+
+    if(logg === '1'){
+      setIsLoggedIn(true);
+    }
+
+  }, [])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('loginstatus','1');
+    setIsLoggedIn(true);
+
+   
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('loginstatus')
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+      }}
+      >
+      <MainHeader  onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+      </AuthContext.Provider>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Login/Login.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useState, useReducer } from "react";
+
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
+
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+
+  return { value: "", isValid: false };
+};
+
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 }
+  }
+
+};
+
+const Login = (props) => {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, emailDispatch] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
+
+  const [passwordState, passwordDispatch] = useReducer(passwordReducer, {
+    value: "",
+    isValid: null,
+  });
+const {isValid: emailIsValidate} = emailState;
+const {isValid: passwordIsValid} = passwordState;
+
+    useEffect(()=>{
+  //usetime out to wait execution a littile longer not to execute with every key stroke
+
+      const identifire = setTimeout(()=>{
+        console.log("idd")
+        setFormIsValid(
+          emailIsValidate && passwordIsValid
+        );
+      },1000)
+      return ()=>{
+        console.log('clean up')
+        clearTimeout(identifire);
+      }
+
+    },[emailIsValidate,passwordIsValid]);
+
+  const emailChangeHandler = (event) => {
+    emailDispatch({ type: "USER_INPUT", val: event.target.value });
+
+    // setFormIsValid(
+    //   emailState.value.includes("@") && passwordState.isValid
+    // );
+  };
+
+  const passwordChangeHandler = (event) => {
+    passwordDispatch({type: "USER_INPUT", val: event.target.value});
+
+    // setFormIsValid(
+    //   emailState.value.includes("@") && passwordState.value.trim().length > 6
+    // );
+  };
+
+  const validateEmailHandler = () => {
+    emailDispatch({ type: "INPUT_BLUR" });
+  };
+
+  const validatePasswordHandler = () => {
+   passwordDispatch({type: "INPUT_BLUR"})
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(emailState.value, passwordState.value);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${
+            emailState.isValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={emailState.value}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${
+            passwordState.isValid === false ? classes.invalid : ""
+          }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={passwordState.value}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
